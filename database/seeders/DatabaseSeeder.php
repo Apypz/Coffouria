9bc2930cb09c87c507ba79e755ac4d1aea5219e9
Apypz <semartpeople@gmail.com>
2025-10-21 12:10:42 +0700

    update besar

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,11 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            // 'category' => 'Hai',
+            // 'name' => 'Aduh',
+            // 'photo' => '',
+            // 'stok' => '',
+            // 'price' => '',
         ]);
     }
 }
